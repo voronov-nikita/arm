4355b27b2ea3d38128ddbe3be279424fd2888c53
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-01-28 23:30:50 +0300

    add base documentation files and clear main.ino

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,8 +1,27 @@
-void setup(){
-  Serial.begin(9600);
-  pinMode(3, INPUT);
-}
+#define NUM_LEDS 72
+#include "FastLED.h"
+#define PIN 6
 
-void loop(){
-  Serial.println(!digitalRead(3));
+CRGB leds[NUM_LEDS];
+byte counter;
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+  FastLED.setBrightness(50);
+  pinMode(13, OUTPUT);
+}
+void loop() {
+  int num = Serial.parseInt();
+  Serial.println(num);
+  // for (int i = 0; i < NUM_LEDS; i++ ) {         // от 0 до первой трети
+  //   leds[i] = CHSV(counter + i * 2, 255, 255);  // HSV. Увеличивать HUE (цвет)
+  //   // умножение i уменьшает шаг радуги
+  // }
+  for (int i; i<NUM_LEDS; i++) leds[i] = CHSV(650, 255, 255);
+  counter++;        // counter меняется от 0 до 255 (тип данных byte)
+  FastLED.show();
+  delay(1);         // скорость движения радуги
 }
