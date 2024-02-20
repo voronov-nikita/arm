d222fa84b7f0b7742d8ad843fedf46108d5fcf59
voronov <114691383+voronov-nikita@users.noreply.github.com>
2024-02-21 00:11:20 +0300

    update files

--- src/receiver/receiver.ino
+++ src/receiver/receiver.ino
@@ -1,29 +1,66 @@
-#include <SPI.h>
-#include <RF24.h>
+int pinEN = 8;
+int dirPinX = 2;
+int stepPinX = 5;
 
-// Указываем пины для подключения модуля nRF24L01
-const int cePin = 9;
-const int csPin = 10;
+int stepsPerRevolution = 200;  // 1/1 
+// int stepsPerRevolution = 400;  // 1/2  
+//int stepsPerRevolution = 1600; // 1/8 
+// int stepsPerRevolution = 3200;   // 1/16 
 
-// Создаем объект RF24 для работы с модулем
-RF24 radio(cePin, csPin);
+int dataSerial = 0;
+int nilai = 0;
+int nilaiDelay = 500;
 
 void setup() {
   Serial.begin(9600);
-
-  // Инициализируем модуль nRF24L01
-  radio.begin();
-
-  // Открываем канал для передачи данных (должен совпадать с каналом на приемной стороне)
-  radio.openWritingPipe(0xF0F0F0F0E1LL);  // Уникальный адрес для передачи данных
+  pinMode(stepPinX, OUTPUT);
+  pinMode(dirPinX, OUTPUT);
+  pinMode(pinEN, OUTPUT);
 }
 
-void loop() {
-  // Отправляем сообщение
-  const char* message = "Hello, Arduino!";
-  radio.write(message, strlen(message));
+void loop()
+{
+  if (Serial.available() != 0)
+  {
+    digitalWrite(pinEN, LOW);       //Aktifkan driver
+    dataSerial = Serial.parseInt();
+    nilai = map(dataSerial, 0, 360, 0, stepsPerRevolution);
 
-  Serial.println("Сообщение отправлено!");
+    Serial.print("Dari Serial:");
+    Serial.print(dataSerial);
+    Serial.print("   Step:");
+    Serial.println(nilai);
+  }
 
-  delay(1000);  // Пауза между отправками
+  //if value +, turn right
+  if (dataSerial > 0)
+  {
+    digitalWrite(dirPinX, LOW);
+
+    for (int i = 0; i < nilai; i++)
+    {
+      digitalWrite(stepPinX, HIGH);
+      delayMicroseconds(nilaiDelay);
+      digitalWrite(stepPinX, LOW);
+      delayMicroseconds(nilaiDelay);
+    }
+
+  }
+
+  //If value -, turn left
+  if (dataSerial < 0)
+  {
+    digitalWrite(dirPinX, HIGH);
+
+    for (int i = 0; i > nilai; i--)
+    {
+      digitalWrite(stepPinX, HIGH);
+      delayMicroseconds(nilaiDelay);
+      digitalWrite(stepPinX, LOW);
+      delayMicroseconds(nilaiDelay);
+    }
+  }
+
+  delay(1000);
+  digitalWrite(pinEN, 0); //Turn off the driver, use this for testing only, to keep the stepper motor from overheating. 
 }
